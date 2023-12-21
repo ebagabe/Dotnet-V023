367ac0eb2baed056eb5ba237e847fc3a9bdd741b
ebagabe <ebagabee@gmail.com>
2023-12-21 15:02:01 -0300

    Enumerate theory

---
+++ Enum1/Enum1/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Enum1.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0, // Se não colocar valor começa com 0 
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3,
+    }
+}
---
+++ Enum1/Enum1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Enum1.Entities;
+using Enum1.Entities.Enums;
+
+Order order = new Order
+{
+    Id = 1080,
+    Moment = DateTime.Now,
+    Status = OrderStatus.PendingPayment
+};
+
+Console.WriteLine(order);
+
+string txt = OrderStatus.PendingPayment.ToString();
+OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+Console.WriteLine(os);
+Console.WriteLine(txt);
