f63319c1b96fee370da34a9b7cd63c6c29fe6c30
ebagabe <ebagabee@gmail.com>
2023-12-21 19:26:01 -0300

    Add Department, enum workerlevel and worker

--- Exercicios/Enum01/Enum01/Program.cs
+++ Exercicios/Enum01/Enum01/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ler os dados de um trabalhador com N contratos
+// Fornecidos pelo usuário
+// Solicitar um mês e mostrar qual foi o salário do funcionario nesse mes
+
+// Enter department's name: 
+
+// Enter worker data:
+
+// Name:
+
+// Level (Junior/MidLevel/Senior): 
+
+// Base salary: 
+
+// How many contracts to this worker? 
+
+// if 3:
+
+// Enter #1 contract data:
+// Date (DD/MM/YYYY): 
+// Value per hour: 
+// Duration (hours): 
+
+// Enter #2 contract data:
+// Date(DD/MM/YYYY): 
+// Value per hour: 
+// Duration (hours): 
+
+// Enter #3 contract data:
+// Date (DD/MM/YYYY): 
+// Value per hour: 
+// Duration (hours): 
+
+// Saída: 
